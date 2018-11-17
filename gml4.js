6c30b4e3955495844605968cfda6e2bb088eecfb
Tiffani <tiffani.grace.elmore@gmail.com>
2018-11-17 14:44:15 -0600

    made testing function

---
+++ gml4.js
@@ -0,0 +1,11 @@
+function assert(expected, actual, msg) {
+    if (expected == actual)
+    {
+        console.log(msg);
+
+    }
+    else {
+        console.log("This is not working");
+    }
+
+}
